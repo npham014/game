39d935e6c50451adfaa842d44f78a95e4dd0f5c8
npham014 <npham014@ucr.edu>
2016-05-19 12:13:16 -0700

    first commit :)

---
+++ Desktop/JSSGame/canvasScript.js
@@ -0,0 +1,211 @@
+
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+context.fillStyle = "black";
+context.fillRect(0, 0, 1062, 550);
+
+var classPicked = false;
+
+//Create Menu Box
+context.beginPath();
+context.lineWidth="10";
+context.strokeStyle="white";
+context.strokeRect(50,400,962,135);
+context.stroke();
+
+
+
+context.font = "30px Arial";
+context.fillStyle = "white";
+//context.textAlign = "center";
+context.fillText("Choose a Class", 425, 100);
+context.fillText("Warrior", 725, 480);
+context.fillText("Mage", 250, 480);
+
+
+//White Dot
+context.beginPath();
+context.lineWidth="10";
+context.strokeStyle="white"; 
+context.strokeRect(225,470,1,1);
+context.stroke();
+
+var classChosen = 1;
+
+var userName = "";
+
+document.addEventListener("keydown", function(e) {
+	console.log(e);
+	if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+    if(!classPicked) {
+    	chooseClass(e);
+    }
+
+});
+
+
+
+//clearBox();
+
+
+function chooseClass(e) {
+
+
+	var userInput = e.keyCode;
+
+	//Begin Idea 1 Below VV
+	//console.log(userInput);
+
+	if(userInput == 37) {
+
+		//Create White Dot
+		context.beginPath();
+		context.lineWidth="10";
+		context.strokeStyle="white";
+		context.strokeRect(225,470,1,1);
+		context.stroke();
+
+
+		//Remove other White Dot
+		context.beginPath();
+		context.lineWidth="10";
+		context.strokeStyle="black";
+		context.strokeRect(700,470,1,1);
+		context.stroke();
+
+		classChosen = 1;
+	}
+	else if(userInput == 39){
+		context.beginPath();
+		context.lineWidth="10";
+		context.strokeStyle="white";
+		context.strokeRect(700,470,1,1);
+		context.stroke();
+
+		context.beginPath();
+		context.lineWidth="10";
+		context.strokeStyle="black";
+		context.strokeRect(225,470,1,1);
+		context.stroke();
+
+		classChosen = 2;
+	}
+	else if(userInput == 13){
+		clearBox();
+		classPicked = true;
+		clearScreen();
+		askName();
+	}
+}
+
+function askName(e) {
+	var nameChosen = false;
+	// var userInput = e.keyCode;
+	context.fillStyle = "white";
+	context.fillText("Enter your name", 425, 100);
+
+	document.addEventListener("keydown", function(x) {
+		console.log(x);
+		if([32, 37, 38, 39, 40].indexOf(x.keyCode) > -1) {
+	        x.preventDefault();
+	    }
+	    if(!nameChosen) {
+	    	chooseName(x);
+	    }
+
+	});
+}
+
+
+function clearBox() {
+	context.beginPath();
+	context.fillStyle="black";
+	context.fillRect(60,410,942,115);
+}
+
+function clearScreen() {
+	context.beginPath();
+	context.fillStyle="black";
+	context.fillRect(0,0,1062, 390);
+}
+
+function chooseName(x) {
+	var userInput = x.keyCode;
+	if(userInput == 13) {
+		userName = $("#mainTextBox").val();
+		clearScreen();
+		$("#mainTextBox").val("");
+		nameChosen = true;
+		beginExposition();
+
+	}
+}
+
+function beginExposition() {
+	
+	drawCharacter();
+	context.font = "20px Arial";
+	context.fillStyle="white";
+	context.fillText("In this world, names hold great power. If one's true name is spoken aloud, the speaker has the power", 80 , 450);
+	context.fillText("over their soul. The very essence of their being. You, brave ", 80 , 475);
+	if(classChosen == 2) {
+		context.fillText("Warrior", 610, 475);
+		context.fillText(userName, 685, 475);
+	}
+	else if(classChosen == 1) {
+		context.fillText("Mage", 610, 475);
+		context.fillText(userName, 670, 475);
+	}
+	context.fillText("are the chosen one, with the power to see the names of all evil-doers. Slay the Evil Necromancer!", 80 , 500);
+	beginGame();
+}
+
+
+function beginGame() {
+	document.addEventListener("keydown", function(y) {
+		console.log(y);
+		if([32, 37, 38, 39, 40].indexOf(y.keyCode) > -1) {
+	        y.preventDefault();
+	    }
+	    var userInput = y.keyCode;
+		if(userInput == 13) {
+			clearBox();
+			moveEnemies();
+		}
+	});
+
+
+}
+
+function drawCharacter() {
+	var smiley = document.getElementById("smiley");
+	context.drawImage(smiley, 10, 10);
+}
+
+var x = 700, //Initialize X starting position
+    velX = 0, //Initialize x starting speed ( i think?)
+    speed = 2,
+    friction = 0.4;
+
+
+function moveEnemies() {
+
+    velX = velX * friction; //Controls speed
+    x += velX;//changes the value of x
+
+    if (x <= 5) { //Make sure X doesn't go off the side
+        x = 5;
+    }
+
+    velX--;
+    clearScreen();
+    context.beginPath();
+    context.fillStyle = "white";
+    context.strokeRect(x,250,1,1);
+    context.stroke();
+
+    setTimeout(moveEnemies, 10);
+
+}
